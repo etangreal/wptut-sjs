086d00b80cd5bc0c9e8486661d55b684098b867f
Ernst Salzmann <ernst.salzmann@datameer.com>
2016-11-10 22:20:30 +0100

    Updates

--- wptut-sjs/app.cb167d85d23bf5a765d6.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(30);console.log("index2.js"),document.body.appendChild(t())},30:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="Hello world :D",n}}});
-//# sourceMappingURL=app.cb167d85d23bf5a765d6.js.map
